0f7c0a503c15cfb42225728f4a24023b14bac01d
Biswajit Dash <bdash8052@gmail.com>
2025-08-30 02:42:43 +0530

    Done hai

--- blogRoutes.js
+++ blogRoutes.js
@@ -1,15 +1,15 @@
 import express from "express";
-import { addBlog,deleteBlogById,getAllBlogs,getBlogByid} from "../controllers/blogController.js";
+import { addBlog, deleteBlogById, getAllBlogs, getBlogByid, togglePublish } from "../controllers/blogController.js";
 import upload from "../middleware/multer.js";
 import auth from "../middleware/auth.js";
-import { deleteBlogById, getBlogByid, togglePublish } from "./blogController.js";
-const blogRouter = express. Router();
 
-blogRouter.post("/add", upload.single('image'), auth, addBlog)
+const blogRouter = express.Router();
+
+blogRouter.post("/add", upload.single('image'), auth, addBlog);
 
 blogRouter.get('/all', getAllBlogs);
 blogRouter.get('/:blogId', getBlogByid);
-blogRouter.post('/delete',auth, deleteBlogById);
-blogRouter.post('/toggle-publish',auth, togglePublish);
+blogRouter.post('/delete', auth, deleteBlogById);
+blogRouter.post('/toggle-publish', auth, togglePublish);
 
 export default blogRouter;
